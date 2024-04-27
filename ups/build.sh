58163939c56efdd75cda30f263301b4829fe9f0f
someone <someone@somewhere.com>
2024-04-27 20:28:00 +0200

    added submodules

--- ups/build.sh
+++ ups/build.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
 
 
-rm -rf sdl/build
-mkdir sdl/build
-cmake -S sdl/SDL -B sdl/build
-cmake --build sdl/build
-cmake --install sdl/build --prefix ../tpl
+# SDL
+
+rm -rf build
+mkdir build
+cmake -S SDL -B build
+cmake --build build
+cmake --install build --prefix ../tpl
+
+# cglm
+
+rm -rf build
+mkdir build
+cmake -S cglm -B build
+cmake --build build
+cmake --install build --prefix ../tpl
+
+# vulkan headers
+
+rm -rf build
+mkdir build
+cmake -S Vulkan-Headers -B build
+cmake --build build
+cmake --install build --prefix ../tpl
+
+# vulkan loader
+
+rm -rf build
+mkdir build
+cmake -S Vulkan-Loader -B build
+cmake --build build
+cmake --install build --prefix ../tpl
 
 
 
